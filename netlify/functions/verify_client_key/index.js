051c1d2fd6a174f1925281953d5faacb3d266a0b
Miles Aylward <maylward@thisisgrow.com>
2021-11-21 10:25:32 -0500

    change client key to use airtable

--- netlify/functions/verify_client_key/index.js
+++ netlify/functions/verify_client_key/index.js
@@ -1,11 +1,24 @@
-const authUser = (data) => new Promise((resolve, reject) => {
-  const isValid = data.key === process.env.RETURNING_CLIENT_KEY;
-  if (isValid) {
-    resolve({ authenticated: true }); 
-  } else {
-    reject({ message: 'Key provided is not valid.' }); 
-  }
+const Airtable = require('airtable');
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base('appPnFPGlYYKyYTxf');
+
+const getKey = () => new Promise((resolve, reject) => {
+  base('Table 1').find('recNVPs96mRyxusfb', (err, record) => {
+    if (err) reject(err)
+    else resolve(record.fields.value);
+  });
 });
+
+const authUser = async (data) => {
+  const key = await getKey();
+  return new Promise((resolve, reject) => {
+    const isValid = data.key === key;
+    if (isValid) {
+      resolve({ authenticated: true }); 
+    } else {
+      reject({ message: 'Key provided is not valid.' }); 
+    }
+  });
+};
 
 const handler = async (event) => {
   try {
